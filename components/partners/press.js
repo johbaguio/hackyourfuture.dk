737d11426f127189d5abd6e686a79f6aff1da44a
behuFTP <benjamin.dals.hughes@gmail.com>
2020-10-26 15:10:56 +0100

    finished about page

--- components/partners/press.js
+++ components/partners/press.js
@@ -2,21 +2,28 @@ import React from 'react'
 import styles from './partners.scss'
 import { useContentfulEntryId } from '../../contentful/contentful-hooks'
 
-export default () =>{
+export default () => {
   const sponsorEntryId = 'LaHuXx6t6IJgELkgbsKhr'
-  const pressPieces = useContentfulEntryId(sponsorEntryId)
-console.log('lol', pressPieces);
-  return (<div className='partners'>
-    <style jsx>{styles}</style>
-    <h2 className='center'>Featured in</h2>
-    <div className='wrapper'>
-      {/* {pressPieces.map(({ id, logo, url, title }) => (
-        <div className='partner' key={id}>
-          <a aria-label='Media link' rel='noopener' target='_blank' href={url}>
-            <img alt={title} src={`/static/media/${logo}`} width='170vw' />
-          </a>
-        </div>
-      ))} */}
+  const pressPieces = useContentfulEntryId(sponsorEntryId).content
+  
+  return (
+    <div className='partners'>
+      <style jsx>{styles}</style>
+      <h2 className='center'>{pressPieces && pressPieces.headline}</h2>
+      <div className='wrapper'>
+        {pressPieces && pressPieces.assets !== 0 && pressPieces.assets.map(({ fields, sys }) => (
+          <div className='partner' key={sys.id}>
+            <a
+              aria-label='Media link'
+              rel='noopener'
+              target='_blank'
+              href={fields.description}
+            >
+              <img alt={fields.title} src={fields.file.url} />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-)}
+  )
+}
