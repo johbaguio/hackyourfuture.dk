47112b040221e80969930521047261917db08855
behuFTP <benjamin.dals.hughes@gmail.com>
2020-10-26 14:38:11 +0100

    first test

--- components/partners/press.js
+++ components/partners/press.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import press from './press.json'
 import styles from './partners.scss'
+import { useContentfulEntryId } from '../../contentful/contentful-hooks'
 
-export default () => (
-  <div className='partners'>
+export default () =>{
+  const sponsorEntryId = 'LaHuXx6t6IJgELkgbsKhr'
+  const pressPieces = useContentfulEntryId(sponsorEntryId)
+console.log('lol', pressPieces);
+  return (<div className='partners'>
     <style jsx>{styles}</style>
     <h2 className='center'>Featured in</h2>
     <div className='wrapper'>
-      {press.map(({ id, logo, url, title }) => (
+      {/* {pressPieces.map(({ id, logo, url, title }) => (
         <div className='partner' key={id}>
           <a aria-label='Media link' rel='noopener' target='_blank' href={url}>
             <img alt={title} src={`/static/media/${logo}`} width='170vw' />
           </a>
         </div>
-      ))}
+      ))} */}
     </div>
   </div>
-)
+)}
--- pages/about.js
+++ pages/about.js
@@ -3,51 +3,52 @@ import marked from 'marked'
 import Layout from '../components/layouts/layout'
 import Content from '../components/layouts/content/content'
 import Map from '../components/map'
-import { content, title } from '../components/content/_about'
-import {
-  content as contact_content,
-  title as contact_title
-} from '../components/content/_contact'
 import Contactform from '../components/contact-form/contact-form'
 import { BoardMembers, CoreTeam } from '../components/team/team'
 import Partners from '../components/partners/partners'
 import Press from '../components/partners/press'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { fetchPageContent } from '../contentful/contentful'
 
-export default () => (
-  <Layout>
-    <Head>
-      <title>{title}</title>
-    </Head>
+export default ({ content, title, contactTitle, contactBody }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+  
+      <Content>
+      <div>{documentToReactComponents(content)}</div>
+      </Content>
+  
+      <Content id='contact'>
+        <h2>{contactTitle}</h2>
+        <div>{documentToReactComponents(contactBody)}</div>
+        <Contactform email={'cph@hackyourfuture.dk'} />
+        <div style={{ marginTop: 40 }}>
+          <Map />
+        </div>
+      </Content>
+  
+      <BoardMembers />
+      <CoreTeam />
+      <Press />
+      <Partners />
+    </Layout>
+  )
+} 
 
-    {/*
-       <Content id='faq'>
-       <h2>Frequently Asked Questions</h2>
-       <div
-       dangerouslySetInnerHTML={{
-       __html: marked(
-       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eaque, explicabo hic id, laborum maiores nisi quae quia quis reprehenderit unde voluptas. Animi aspernatur at in modi natus quidem quis.'
-       )
-       }}
-       />
-       </Content>
-      */}
+export async function getStaticProps() {
+  const pageContent = await fetchPageContent('3VD8j3TGv3H66fVLrHGWuC')
+  const contactContent = await fetchPageContent('57BmNlPMn5pBZBBvPNIoLC')
+  
+  return {
+    props: {
+      title: pageContent.headline,
+      content: pageContent.mainBody,
+      contactTitle: contactContent.title,
+      contactBody: contactContent.bodyText,
 
-    <Content>
-      <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
-    </Content>
-
-    <Content id='contact'>
-      <h2>{contact_title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: marked(contact_content) }} />
-      <Contactform email={'cph@hackyourfuture.dk'} />
-      <div style={{ marginTop: 40 }}>
-        <Map />
-      </div>
-    </Content>
-
-    <BoardMembers />
-    <CoreTeam />
-    <Press />
-    <Partners />
-  </Layout>
-)
+    }
+  }
+}
