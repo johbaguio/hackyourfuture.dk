efb3401df5d6c1422b0f8040db0ec4d8c2455d58
behuFTP <benjamin.dals.hughes@gmail.com>
2020-12-06 15:44:51 +0100

    Updated partners

--- components/partners/partners.js
+++ components/partners/partners.js
@@ -3,48 +3,29 @@ import styles from './partners.scss'
 import { useContentfulEntryId } from '../../contentful/contentful-hooks'
 
 export default () => {
-  const partnersEntryId = '3BMLvRREsbeQcUPbpoqHGS'
-  const sponsorEntryId = '6D2BMNErzLJpTE31AavokM'
+  const partnersEntryId = '6cUNFgR3TLOKOlY8on904p'
   const partners = useContentfulEntryId(partnersEntryId).content
-  const sponsor = useContentfulEntryId(sponsorEntryId).content
 
   return (
     <>
       <style jsx>{styles}</style>
-      {sponsor && partners &&
+      {partners && (
         <section className='partners'>
-          <h2 className='center'>{sponsor.headline}</h2>
-          <a
-            aria-label='Partner link'
-            rel='noopener'
-            target='_blank'
-            href={sponsor.assets[0].fields.description}
-            className='mollerske'
-          >
-            <img
-              alt='Den A.P. Møllerske Støttefond logo'
-              src={sponsor.assets[0].fields.file.url}
-            />
-          </a>
-          <div>
-            <h2 className='center'>{partners.headline}</h2>
-            <div className='wrapper'>
-              {partners.assets.map(partner => (
-                <div className='partner' key={partner.sys.id}>
-                  <a
-                    aria-label='Partner link'
-                    rel='noopener'
-                    target='_blank'
-                    href={partner.fields.description}
-                  >
-                    <img alt={partner.fields.title} src={partner.fields.file.url} width='150vw' />
-                  </a>
-                </div>
-              ))}
-            </div>
-          </div>
+          <h2>{partners.title}</h2>
+          <ul>
+            {partners.images.map(partnerImages => (
+              <li>
+                <a target='_blank' href={partnerImages.fields.link}>
+                  <img
+                    src={partnerImages.fields.image.fields.file.url}
+                    alt={partnerImages.fields.image.description}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
         </section>
-      }
+      )}
     </>
   )
 }
