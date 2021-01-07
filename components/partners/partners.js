1fb7d8fca212466d8e4244f3ed5c58e3b9776649
behuFTP <benjamin.dals.hughes@gmail.com>
2021-01-07 15:24:37 +0100

    add type to partners

--- components/partners/partners.js
+++ components/partners/partners.js
@@ -3,8 +3,19 @@ import styles from './partners.scss'
 import { useContentfulEntryId } from '../../contentful/contentful-hooks'
 
 export default () => {
-  const partnersEntryId = '6cUNFgR3TLOKOlY8on904p'
+  const partnersEntryId = '01vaQwhCtWAhCpI24OScSq'
   const partners = useContentfulEntryId(partnersEntryId).content
+  // console.log(partners)
+
+  const goldPartners =
+    partners &&
+    partners.partners.filter(partner => partner.fields.type === 'gold')
+  const silverPartners =
+    partners &&
+    partners.partners.filter(partner => partner.fields.type === 'silver')
+  const bronzePartners =
+    partners &&
+    partners.partners.filter(partner => partner.fields.type === 'bronze')
 
   return (
     <>
@@ -13,12 +24,36 @@ export default () => {
         <section className='partners'>
           <h2>{partners.title}</h2>
           <ul>
-            {partners.images.map(partnerImages => (
-              <li>
-                <a target='_blank' href={partnerImages.fields.link}>
+            {goldPartners.map(partner => (
+              <li className={partner.fields.type}>
+                <a target='_blank' href={partner.fields.link}>
                   <img
-                    src={partnerImages.fields.image.fields.file.url}
-                    alt={partnerImages.fields.image.description}
+                    src={partner.fields.logo.fields.file.url}
+                    alt={partner.fields.logo.title}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {silverPartners.map(partner => (
+              <li className={partner.fields.type}>
+                <a target='_blank' href={partner.fields.link}>
+                  <img
+                    src={partner.fields.logo.fields.file.url}
+                    alt={partner.fields.logo.title}
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+          <ul>
+            {bronzePartners.map(partner => (
+              <li className={partner.fields.type}>
+                <a target='_blank' href={partner.fields.link}>
+                  <img
+                    src={partner.fields.logo.fields.file.url}
+                    alt={partner.fields.logo.title}
                   />
                 </a>
               </li>
